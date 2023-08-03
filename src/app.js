424bd5d733c43a3d6a0b8063c5a812ba04034976
mariajazmin <lamasmariajazmin@gmail.com>
2023-08-03 05:58:53 -0300

    home y register

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require ("express");
+const app = express ();
+const path = require ("path");
+
+app.use(express.static("public"));
+
+app.listen(3000, ()=>"servidor escuchando en el puerto 3000!");
+
+app.get("/", (req, res)=>{
+    res.sendFile(path.join(__dirname, "./views/index.html"));
+});
+
+app.get("/formulario-de-registro", (req, res)=>{
+    res.sendFile(path.join(__dirname, "./views/formulario-de-registro.html"));
+});
