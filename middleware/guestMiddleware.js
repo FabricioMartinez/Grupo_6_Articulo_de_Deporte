b9222de77bb31d586566ac163893a5df88f5cbd8
angie734 <43140244@fi.unju.edu.ar>
2023-10-05 00:14:07 -0300

    cambios en rutas huespedes

--- middleware/guestMiddleware.js
+++ middleware/guestMiddleware.js
@@ -1,7 +1,7 @@
 //Middleware para rutas accesibles solo sin login
 const guestMiddleware = (req, res, next) => {
   if (req.session.userLogged) {
-    return res.redirect("/user/profile"); // Redirige al perfil si el usuario está logueado
+    return res.redirect("/user/profile"); 
   }
   next();
 };
@@ -9,7 +9,7 @@ const guestMiddleware = (req, res, next) => {
 // Middleware para rutas accesibles solo con login
 const authMiddleware = (req, res, next) => {
   if (!req.session.userLogged) {
-    return res.redirect("/user/login"); // Redirige al login si el usuario no está logueado
+    return res.redirect("/user/login");
   }
   next();
 };
